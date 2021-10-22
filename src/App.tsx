3115665157c469d3b674aa6acefaaad68bcacb72
João Paulo Moraes <joaopaulomoraes@outlook.com>
2021-10-22 14:41:39 -0300

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { Switch, Route, Link } from 'react-router-dom';
+
+import Button from '@eduzz/houston-ui/Button';
+
 import Welcome from '@/components/Welcome';
 
 export default function App() {
-  return <Welcome to='App name' />;
+  return (
+    <Switch>
+      <Route path='/' exact>
+        <Link to='/about'>
+          <Button variant='text'>About</Button>
+        </Link>
+        <Welcome to='App name' />
+      </Route>
+      <Route path='/about'>
+        <Link to='/'>
+          <Button variant='text'>Home</Button>
+        </Link>
+        <Welcome to='About' />
+      </Route>
+    </Switch>
+  );
 }
